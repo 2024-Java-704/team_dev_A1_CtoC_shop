942039751ca083e9bfbd2dec52b11e77766ecd27
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-06 16:54:49 +0900

    Update ItemController

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -8,22 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Item;
+import com.example.demo.model.Account;
 import com.example.demo.repository.ItemRepository;
 
 public class ItemController {
-	
+
 	@Autowired
 	ItemRepository itemRepository;
-	
+
 	@Autowired
 	Account account;
-	
+
 	@GetMapping("/home")
 	public String textList(
-							@RequestParam(name="keyword",defaultValue="") String keyword,
-							Model model) {
-		List<Item>ItemList=null;
-		Integer buyerId=account.getid();
+			@RequestParam(name = "keyword", defaultValue = "") String keyword,
+			Model model) {
+		List<Item> ItemList = null;
+		Integer buyerId = account.getId();
 		
-}
+		return "home";
+	}
 }
