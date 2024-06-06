95b82f052a39f197d14a23b6830ef6700ea5ca93
ishikawa-seiga-6494 <420-student054@la-study.com>
2024-06-06 15:47:20 +0900

    Itemに関するEntity,Repositoryの追加

---
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Item;
+import com.example.demo.repository.ItemRepository;
+
+public class ItemController {
+	
+	@Autowired
+	ItemRepository itemRepository;
+	
+	@Autowired
+	Account account;
+	
+	@GetMapping("/home")
+	public String textList(
+							@RequestParam(name="keyword",defaultValue="") String keyword,
+							Model model) {
+		List<Item>ItemList=null;
+		Integer buyerId=account.getid();
+		
+}
+}
---
+++ src/main/java/com/example/demo/entity/Item.java
@@ -0,0 +1,64 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="items")
+public class Item {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Integer id; //商品ID
+
+@Column(name="textbook_id")
+private Integer textbookId; //教科書ID
+
+@Column(name="item_status")
+private Integer itemStatus; //商品状態　1＝新品、２＝中古、3＝書き込みあり、4＝破損あり
+
+@Column(name="deal_status")
+private Integer dealStatus; //取引状態　1＝審査待ち、2＝不許可、3＝出品中（許可）、4＝取引中、5＝完了
+
+@Column(name="seller_id")
+private Integer sellerId; //出品者ID
+
+@Column(name="buyer_id")
+private Integer buyerId; //購入者ID
+
+//コンストラクタ
+Item(){ //デフォルトコンストラクタ	
+}
+Item(Integer id,Integer textbookId,Integer itemStatus,Integer dealStatus,Integer sellerId,Integer buyerId){
+	this.id=id;
+	this.textbookId=textbookId;
+	this.itemStatus=itemStatus;
+	this.dealStatus=dealStatus;
+	this.sellerId=sellerId;
+	this.buyerId=buyerId;
+}
+
+//メソッド
+public Integer getId() {
+	return id;
+}
+public Integer getTextbookId() {
+	return textbookId;
+}
+public Integer getItemStatus() {
+	return itemStatus;
+}
+public Integer getDealStatus() {
+	return dealStatus;
+}
+public Integer getSellerId() {
+	return sellerId;
+}
+public Integer getBuyerId() {
+	return buyerId;
+}
+
+}
