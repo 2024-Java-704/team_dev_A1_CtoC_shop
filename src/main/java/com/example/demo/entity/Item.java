8f3f394cd4cb8cd030f47f3e003021db03927e34
ishikawa-seiga-6494 <420-student054@la-study.com>
2024-06-07 10:19:52 +0900

    Itemの変更

--- src/main/java/com/example/demo/entity/Item.java
+++ src/main/java/com/example/demo/entity/Item.java
@@ -32,13 +32,11 @@ private Integer buyerId; //購入者ID
 //コンストラクタ
 Item(){ //デフォルトコンストラクタ	
 }
-Item(Integer id,Integer textbookId,Integer itemStatus,Integer dealStatus,Integer sellerId,Integer buyerId){
-	this.id=id;
+Item(Integer textbookId,Integer itemStatus,Integer sellerId){
 	this.textbookId=textbookId;
 	this.itemStatus=itemStatus;
-	this.dealStatus=dealStatus;
 	this.sellerId=sellerId;
-	this.buyerId=buyerId;
+	this.dealStatus=1;
 }
 
 //メソッド
