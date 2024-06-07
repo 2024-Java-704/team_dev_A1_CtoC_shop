3c74e1eb8fd688c0ef4c548498901967d7f22bcd
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-07 11:48:00 +0900

    ItemController

--- src/main/java/com/example/demo/repository/TextbookRepository.java
+++ src/main/java/com/example/demo/repository/TextbookRepository.java
@@ -8,7 +8,7 @@ import com.example.demo.entity.Textbook;
 
 public interface TextbookRepository extends JpaRepository<Textbook, Integer>{
 
-	List<Textbook> findByNameLike(String keyword);
+	List<Textbook> findByTitleLike(String keyword);
 
 	Textbook findOneById(Integer textbookId);
 
