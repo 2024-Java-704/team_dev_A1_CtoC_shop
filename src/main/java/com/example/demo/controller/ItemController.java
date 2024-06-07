269b61c1516c7fd19d270e8ddb7d72e86ed5d3d8
ishikawa-seiga-6494 <420-student054@la-study.com>
2024-06-07 17:02:26 +0900

    Controllerの追加

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Item;
@@ -35,29 +36,85 @@ public class ItemController {
 
 	@GetMapping("/home")
 	public String textList(
-
-
-			@RequestParam(name = "keyword", defaultValue = "") String keyword,
-			Model model) {
+						   @RequestParam(name = "keyword", defaultValue = "") String keyword,
+						   Model model) {
 		List<Item> itemList = null;
 		List<Textbook> bookList =null;
-		List<ItemImage> itemImage = null;
-		List<Textbook> textbook = null;
-		ArrayList<Integer>textIdList=new ArrayList<>();
+		List<ItemImage> itemImage=new ArrayList<>();
+		List<Textbook> textbook=new ArrayList<>();
 		if(keyword.length()==0 ||keyword.equals(null)) {
 			itemList=itemRepository.findDistinctByTextbookIdOrderByAsc();
 			for(Item itemlist :itemList ) {
 				itemImage.add(itemImageRepository.findDistinctByItemId(itemlist.getId()));
-		//	textbook.add(textbookRepository.findById(itemlist.getTextbookId()));
+				textbook.add(textbookRepository.findById(itemlist.getTextbookId()).get());
 			}
 		}else {
-			bookList=textbookRepository.findByTitleLike(keyword);
-			/*	for( itemlist :itemList ) {
-			 }
-			 */
+			bookList=textbookRepository.findByTitleLikeOrderByAsc(keyword);
+				for(Textbook booklist :bookList ) {
+					itemList=itemRepository.findDistinctByTextbookIdOrderByAsc(booklist.getId());
+					for(Item itemlist :itemList ) {
+						itemImage.add(itemImageRepository.findDistinctByItemId(itemlist.getId()));
+						textbook.add(textbookRepository.findById(itemlist.getTextbookId()).get());
+					}
+				}
 		}
 		model.addAttribute("keyword", keyword);
 		model.addAttribute("itemList", itemList);
+		model.addAttribute("itemImage", itemImage);
+		model.addAttribute("textbook",textbook);
 		return "home";
+	}
+	
+	@GetMapping("/textbook/{id}")
+	public String itemList(
+						   @PathVariable("id")Integer id,
+						   @RequestParam(name="sort", defaultValue="1")Integer sort,
+						   Model model){
+		List<Item> itemList = null;
+		//List<Textbook> bookList =null;
+		List<ItemImage> itemImage=new ArrayList<>();
+		List<Textbook> textbook=new ArrayList<>();
+		if(sort==1) {
+			itemList=itemRepository.findByTextbookIdOrderByDesc(id);
+			for(Item itemlist :itemList ) {
+				itemImage.add(itemImageRepository.findDistinctByItemId(itemlist.getId()));
+				textbook.add(textbookRepository.findById(itemlist.getTextbookId()).get());
+			}
+				}else if(sort==2) {
+					itemList=itemRepository.findByTextbookIdOrderByitemStatusDesc(id);
+					for(Item itemlist :itemList ) {
+					itemImage.add(itemImageRepository.findDistinctByItemId(itemlist.getId()));
+					textbook.add(textbookRepository.findById(itemlist.getTextbookId()).get());
+			}
+				}else if(sort==3) {
+					itemList=itemRepository.findByTextbookIdOrderByitemStatusAsc(id);
+					for(Item itemlist :itemList ) {
+					itemImage.add(itemImageRepository.findDistinctByItemId(itemlist.getId()));
+					textbook.add(textbookRepository.findById(itemlist.getTextbookId()).get());
+			}
+				}
+					model.addAttribute("itemList", itemList);
+					model.addAttribute("itemImage", itemImage);
+					model.addAttribute("textbook",textbook);
+			return "textbook";
+	}
+	@GetMapping("/item/{id}")
+	public String viewItem(
+						   @PathVariable("id")Integer id,
+						   Model model) {
+		Item item = null;
+		List<ItemImage> itemImage=new ArrayList<>();
+			item=itemRepository.findById(id).get();
+			itemImage.add(itemImageRepository.findByItemId(item.getId()));
+			return "item";
+	}
+	@GetMapping("/user/{id}")
+	public String viewUser(
+						   @PathVariable("id")Integer id,
+						   Model model) {
+		List<Item> itemList = null;
+		List<Textbook> bookList =null;
+		//	itemList=itemRepository.findBySellerId(id);
+		return"";
 	}
 }
--- src/main/java/com/example/demo/repository/TextbookRepository.java
+++ src/main/java/com/example/demo/repository/TextbookRepository.java
@@ -8,7 +8,9 @@ import com.example.demo.entity.Textbook;
 
 public interface TextbookRepository extends JpaRepository<Textbook, Integer>{
 
-	List<Textbook> findByTitleLike(String keyword);
+	List<Textbook> findByTitleLikeOrderByAsc(String keyword);
+
+	Object findOneById(Integer textbookId);
 
 
 }
