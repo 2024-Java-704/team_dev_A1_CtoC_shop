49162849ec1e856713783cea5f52ac1adb328ea4
江嵜　遊太 <166344399+esaki-yuta-6506@users.noreply.github.com>
2024-06-10 10:12:31 +0900

    Update TextbookRepository.java

--- src/main/java/com/example/demo/repository/TextbookRepository.java
+++ src/main/java/com/example/demo/repository/TextbookRepository.java
@@ -8,12 +8,4 @@ import com.example.demo.entity.Textbook;
 
 public interface TextbookRepository extends JpaRepository<Textbook, Integer>{
 
-
-	List<Textbook> findByNameLike(String keyword);
-
-	List<Textbook> findByTitleLikeOrderByAsc(String keyword);
-
-	Object findOneById(Integer textbookId);
-
-
 }
