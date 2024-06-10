b3e78ad8114514fce8bc5e339d42250b36476082
mitsui-yutaro-6379 <429-student040@la-study.com>
2024-06-10 11:34:40 +0900

    ItemRepositoryの修正

--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -9,22 +9,29 @@ import com.example.demo.entity.Item;
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 	List<Item> findBySellerIdOrderByIdDesc(Integer id);
+
 	List<Item> findByBuyerIdOrderByIdDesc(Integer id);
 
 	Item findOneById(Integer id);
 
-	List<Item> findDistinctByTextbookIdOrderByAsc();
-
-	List<Item> findDistinctByTextbookIdOrderByIdAsc();
-
 	List<Item> findDistinctByTextbookIdOrderByIdAsc(Integer id);
 
-	List<Item> findByTextbookIdOrderByIdDesc(Integer textBookid);
+	List<Item> findByTextbookIdOrderByIdDesc(Integer textbookId);
+
+	List<Item> findByTextbookIdOrderByItemStatusDesc(Integer textbookId);
+
+	List<Item> findByTextbookIdOrderByItemStatusAsc(Integer textbookId);
+
+	List<Item> findDistinctByTextbookIdOrderByIdAsc();
 
 	List<Item> findByTextbookIdOrderByitemStatusDesc(Integer id);
 
 	List<Item> findByTextbookIdOrderByitemStatusAsc(Integer id);
 
 	List<Item> findByDealStatus(int dealStatus);
+
+	List<Item> findAllByOrderByTextbookIdDesc();
+
+	List<Item> findByDealStatus(Integer dealStatus);
 
 }
