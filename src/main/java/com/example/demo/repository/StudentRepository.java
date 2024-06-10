9a8432da3cd7cfa9248ed8037704141c664793e2
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-10 15:50:34 +0900

    StudentRepository

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+	Student findOneByStudentNumber(String studentNumber);
 }
