952cd83f88103d29b9986e26775eb45a811bb442
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-10 11:17:20 +0900

    ItemRepository

--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -9,13 +9,16 @@ import com.example.demo.entity.Item;
 public interface ItemRepository extends JpaRepository<Item,Integer>{
 
 
-	List<Item> findBySellerIdOrderByIdDesc(Integer sellerId);
-	
-	List<Item> findByBuyerIdOrderByIdDesc(Integer buyerId);
+	List<Item> findBySellerIdOrderByIdDesc(Integer id);
+	List<Item> findByBuyerIdOrderByIdDesc(Integer id);
 
+	
+	
 	Item findOneById(Integer id);
 
-	List<Item> findDistinctByTextbookIdOrderByIdAsc(Integer textbookId);
+	
+	
+	List<Item> findDistinctByTextbookIdOrderByIdAsc(Integer id);
 	
 	List<Item> findByTextbookIdOrderByIdDesc(Integer textbookId);
 
@@ -23,6 +26,12 @@ public interface ItemRepository extends JpaRepository<Item,Integer>{
 
 	List<Item> findByTextbookIdOrderByItemStatusAsc(Integer textbookId);
 
+	
+	List<Item> findAllByOrderByTextbookIdDesc();
+	
+	
+	
+	
 	List<Item> findByDealStatus(Integer dealStatus);
 
 }
