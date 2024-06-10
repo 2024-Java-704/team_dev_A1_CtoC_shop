ed60674f05630ca240e7ef4b872d5e870159ee47
watanabe-tetuya-6492 <420-student005@la-study.com>
2024-06-10 11:33:55 +0900

    HTMLの作成およびAccountControllerの更新

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -185,8 +185,8 @@ public class AccountController {
 	public String resetIntroduce(Model model) {
 		//idでuserを検索
 		User user = userRepository.findById(account.getId()).get();
-		//検索したユーザーの自己紹介をoldIntroduceとして送信
-		model.addAttribute("oldIntroduce", user.getIntroduce());
+		//検索したユーザーの学籍番号を送信
+		model.addAttribute("studentNumber", user.getStudentNumber());
 		//自己紹介更新ページを開く
 		return "resetIntroduce";
 	}
