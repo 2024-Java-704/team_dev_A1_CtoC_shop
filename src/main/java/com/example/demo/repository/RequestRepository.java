28a946ffa9e1c659e912e7c18a462e4aee1aef50
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-11 10:47:55 +0900

    AdminController NoticeAlate

--- src/main/java/com/example/demo/repository/RequestRepository.java
+++ src/main/java/com/example/demo/repository/RequestRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Request;
 
 public interface RequestRepository extends JpaRepository<Request, Integer> {
+
+	List<Request> findByTextbookId(Integer textbookId);
 
 }
