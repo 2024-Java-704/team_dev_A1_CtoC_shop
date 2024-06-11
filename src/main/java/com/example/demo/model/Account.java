a807b89a862185556390070c504a9de68d0a7635
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-11 09:47:38 +0900

    header footer

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,18 +1,29 @@
 package com.example.demo.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
+
+import com.example.demo.repository.ClaimRepository;
+import com.example.demo.repository.NoticeRepository;
 
 //Modelです
 @Component
 @SessionScope
 public class Account {
+
+	@Autowired
+	NoticeRepository noticeRepository;
+
+	@Autowired
+	ClaimRepository claimRepository;
+
 	private Integer id;
 	private Integer userStatus;
 
-	public Account() {	
+	public Account() {
 	}
-	
+
 	public Account(Integer id, Integer userStatus) {
 		this.id = id;
 		this.userStatus = userStatus;
@@ -32,5 +43,13 @@ public class Account {
 
 	public Integer getUserStatus() {
 		return userStatus;
+	}
+
+	public Integer getNoticeCount() {
+		return noticeRepository.findByUserIdAndNoticeStatus(id, 1).size();
+	}
+
+	public Integer getClaimCount() {
+		return claimRepository.findByClaimStatus(1).size();
 	}
 }
