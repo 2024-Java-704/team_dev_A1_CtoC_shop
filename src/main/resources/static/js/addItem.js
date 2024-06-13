f0cee5cdd2b1537033ac4523058cacad0e56141b
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-13 10:19:00 +0900

    fix

--- src/main/resources/static/js/addItem.js
+++ src/main/resources/static/js/addItem.js
@@ -1,32 +1,35 @@
 //複数画像選択
 
- 		// 選択した画像ファイルのプレビューを生成する関数を定義します。
-        function preview(obj) {
-            const element = document.getElementsByClassName('sampleImage'); 
+	// 選択した画像ファイルのプレビューを生成する関数を定義します。
+	function preview(obj) {
+		setTimeout(() => {
+			const element = document.getElementsByClassName('sampleImage'); 
 			while (element.length) {
- 				 element.item(0).remove()
+				element.item(0).remove()
 			}
-            // 選択した全てのファイルに対してループ処理をします。
-            for (i = 0; i < obj.files.length; i++) {
-                // FileReaderオブジェクトを作成します。これによりブラウザ上でファイルを読み込むことが可能になります。
-                let fileReader = new FileReader();
-                // onloadイベントハンドラを設定します。ファイルが正常に読み込まれたときに呼び出されます。
-                fileReader.onload = ((e)=> {
-                    // 読み込んだ画像ファイルをData URLとしてimg要素に設定します。
-                    // これにより、選択した画像がブラウザ上でプレビュー表示されます。
-                    document.querySelector('.img').innerHTML += '<img src="' + e.target.result + '" class="sampleImage">';
-                });
-                // ファイルをData URLとして読み込みます。
-                fileReader.readAsDataURL(obj.files[i]);
-                //ボタンを活性化
-            }
-			if(obj.files.length){
+			// 選択した全てのファイルに対してループ処理をします。
+			for (i = 0; i < obj.files.length; i++) {
+				// FileReaderオブジェクトを作成します。これによりブラウザ上でファイルを読み込むことが可能になります。
+				let fileReader = new FileReader();
+				// onloadイベントハンドラを設定します。ファイルが正常に読み込まれたときに呼び出されます。
+				fileReader.onload = ((e)=> {
+					// 読み込んだ画像ファイルをData URLとしてimg要素に設定します。
+					// これにより、選択した画像がブラウザ上でプレビュー表示されます。
+					document.querySelector('.img').innerHTML += '<img src="' + e.target.result + '" class="sampleImage">';
+				});
+				// ファイルをData URLとして読み込みます。
+				fileReader.readAsDataURL(obj.files[i]);
+				//ボタンを活性化
+			}
+			if(obj.files.length > 0){
 				const button = document.getElementById("submitButton");
 				button.disabled = null;
 			}else {
+				const button = document.getElementById("submitButton");
 				button.disabled = "disabled";
 			}
-        }
+		}, 500);
+	}
         
        
 //ファイル名非表示       
