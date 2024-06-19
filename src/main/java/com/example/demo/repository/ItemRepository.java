1136eb11b7eeae986780a39a857b20e31303f3d1
esaki-yuta-6506 <420-student@012@la-study.com>
2024-06-19 11:55:52 +0900

    mypageにレビュー表示

--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -28,4 +28,6 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 	List<Item> findByTextbookIdAndDealStatusOrderByTextbookIdDesc(Integer textbookId, Integer dealStatus);
 
+	List<Item> findBySellerIdAndDealStatus(Integer sellerId, Integer dealStatus);
+
 }
